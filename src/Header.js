0af4de2012437bbbf8f3ce946f2d9edece7cf5b8
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-14 21:28:16 +0100

    header options

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,32 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import { BusinessCenter, Chat, Notifications } from "@mui/icons-material";
 
 function Header() {
   return (
     <div className="header">
-      <h2>This is the header</h2>
-
       <div className="header__left">
-        <img src="" alt="" />
+        <img
+          src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1655237840~hmac=f397c18e3279cbd425feb10290e0682a"
+          alt="linkedIn"
+        />
         <div className="header__search">
           <SearchIcon />
           <input type="text" />
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenter} title="Jobs" />
+        <HeaderOption Icon={Chat} title="Messaging" />
+        <HeaderOption Icon={Notifications} title="Notifications" />
+      </div>
     </div>
   );
 }
