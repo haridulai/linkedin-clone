13693f3af3983022af365ccfc8265205f10290a0
hdcodesdev <91739271+hdcodesdev@users.noreply.github.com>
2022-06-15 10:59:29 +0100

    redux login functionality

--- src/Header.js
+++ src/Header.js
@@ -16,7 +16,7 @@ function Header() {
         />
         <div className="header__search">
           <SearchIcon />
-          <input type="text" />
+          <input type="text" placeholder="Search" />
         </div>
       </div>
 
